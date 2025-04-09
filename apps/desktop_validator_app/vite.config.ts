55c774683e32c8b990d1c264ce1bc960a1eb1535
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2025-04-09 16:23:10 +0530

    vite react added for electron app

---
+++ apps/desktop_validator_app/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import type { PluginOption } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react() as PluginOption],
+  build: {
+    outDir: 'dist-react',
+  },
+})
